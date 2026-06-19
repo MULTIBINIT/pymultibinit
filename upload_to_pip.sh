c62ea8a033aa1687f845fe31031d055451c5d402
mailhexu <mailhexu@gmail.com>
2026-06-19 23:35:38 +0200

    Release 0.3.2
    Bumps version from 0.3.1 to 0.3.2, adds LICENSE, excludes memnotes/ vault from packaging via .gitignore, and switches the PyPI upload script to use uv build.

--- upload_to_pip.sh
+++ upload_to_pip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 rm ./dist/*
-python3 -m build
-python3 -m twine upload --repository pypi dist/* --verbose
+#python3 -m build
+#python3 -m twine upload --repository pypi dist/* --verbose
+uv build
+uv run --with twine python -m twine upload --repository pypi dist/* --verbose
